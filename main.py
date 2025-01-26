b36d414cf29c78a1814ef5201cd3ec1035ee8e63
JJ <4843736+espin086@users.noreply.github.com>
2025-01-26 07:56:58 -0800

    feature: added tracing to the llm call

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-
+"""
+This script generates a medium article about a given topic.
+"""
+import openai 
+from prompts.WriteArticlePrompt import prompt_template
 from langchain_openai import ChatOpenAI
-from prompts.WriteArticlePrompt import WriteArticlePrompt
+from langsmith import wrappers, traceable
 
 
 
+# auto-trace LLM calls in-context
+client = wrappers.wrap_openai(openai.Client())
 
-print(WriteArticlePrompt.invoke({"topic":"The future of AI in healthcare"}))
+@traceable
+def generate_article(topic:str, model:str="gpt-3.5-turbo", temperature:float=0) -> str:
+    """
+    Generate an medium article about a given topic.
+    """
+    llm = ChatOpenAI(model=model, temperature=temperature)
+    chain = prompt_template | llm
+    return chain.invoke({"topic":topic})
+
+
+
+if __name__ == "__main__":
+    print(generate_article("The future of AI in healthcare"))
+
+
+
--- prompts/WriteArticlePrompt.py
+++ prompts/WriteArticlePrompt.py
@@ -1,11 +1,10 @@
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
-WriteArticlePrompt = PromptTemplate.from_template(
-    """
-    You are a writer for a tech blog. You are given a topic and you need to write an article about it.
-    Here is the topic: {topic}
-    """
-)
+prompt_template = ChatPromptTemplate([
+    ("system", "You are a helpful assistant"),
+    ("user", "Write a medium article about {topic}")
+])
+
 
 if __name__ == "__main__":
-    print(WriteArticlePrompt.invoke({"topic":"The future of AI in healthcare"}))
+    print(prompt_template.invoke({"topic": "cats"}))
